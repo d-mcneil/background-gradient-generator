380501c3a7a8ba8f43cd7969c52164b47a645f93
Dean McNeil <deanolmcneilgmail.com>
2022-09-01 13:18:57 -0500

    Update syntax

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-var cssCode = document.getElementById("hexcolor");
-var color1 = document.getElementById("color1");
-var color2 = document.getElementById("color2");
-var body = document.querySelector("body");
-var randomizeButton = document.getElementById("random");
-var copyButton = document.getElementById("copy");
+let cssCode = document.getElementById('hexcolor');
+let color1 = document.getElementById('color1');
+let color2 = document.getElementById('color2');
+const body = document.querySelector('body');
+const randomizeButton = document.getElementById('random');
+const copyButton = document.getElementById('copy');
 
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    cssCode.textContent = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = `background: linear-gradient(to right, ${color1.value}, ${color2.value});`;
 }
 
+const generateRandomHex = () => Math.round(Math.random() * 255).toString(16);
+
 function randomize() {
-    var colorNumbers = [
-        Math.round(Math.random() * 255).toString(16),
-        Math.round(Math.random() * 255).toString(16),
-        Math.round(Math.random() * 255).toString(16),
-        Math.round(Math.random() * 255).toString(16),
-        Math.round(Math.random() * 255).toString(16),
-        Math.round(Math.random() * 255).toString(16)
+    let colorNumbers = [
+        generateRandomHex(),
+        generateRandomHex(),
+        generateRandomHex(),
+        generateRandomHex(),
+        generateRandomHex(),
+        generateRandomHex()
     ];
     for (var i = 0; i < colorNumbers.length; i++) {
         if (colorNumbers[i].length === 1) {
-            colorNumbers[i] = "0" + colorNumbers[i];
+            colorNumbers[i] = `0${colorNumbers[i]}`;
         }
     }
-    color1.value = "#" + colorNumbers[0] + colorNumbers[1] + colorNumbers[2];
-    color2.value = "#" + colorNumbers[3] + colorNumbers[4] + colorNumbers[5];
+    color1.value = `#${colorNumbers[0]}${colorNumbers[1]}${colorNumbers[2]}`;
+    color2.value = `#${colorNumbers[3]}${colorNumbers[4]}${colorNumbers[5]}`;
     setGradient();
 }
 
@@ -34,7 +36,7 @@ function copyCode() {
 }
 
 setGradient();
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
-randomizeButton.addEventListener("click", randomize);
-copyButton.addEventListener("click", copyCode);
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
+randomizeButton.addEventListener('click', randomize);
+copyButton.addEventListener('click', copyCode);
